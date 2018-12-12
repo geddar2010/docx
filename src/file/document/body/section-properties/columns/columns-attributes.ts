c2174fceba913cc4cbc9e4222ea8db2acf5190df
geddar2010 <geddar2010@gmail.com>
2018-12-12 12:28:01 +0300

    columns

--- src/file/document/body/section-properties/columns/columns-attributes.ts
+++ src/file/document/body/section-properties/columns/columns-attributes.ts
@@ -2,10 +2,16 @@ import { XmlAttributeComponent } from "file/xml-components";
 
 export interface IColumnsAttributes {
     readonly space?: number;
+    readonly equalWidth?: boolean;
+    readonly num?: number;
+    readonly sep?: boolean;
 }
 
 export class ColumnsAttributes extends XmlAttributeComponent<IColumnsAttributes> {
     protected readonly xmlKeys = {
         space: "w:space",
+        equalWidth: "w:equalWidth",
+        num: "w:num",
+        sep:"w:sep"
     };
 }
