5425dacb971f134b4955d188e3b144f27ec5c2c5
geddar2010 <geddar2010@gmail.com>
2018-12-12 17:28:09 +0300

    build

--- build/file/styles/latent-styles/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { XmlComponent } from "../../../file/xml-components";
-import { LatentStyleException } from "./exceptions";
-export declare class LatentStyles extends XmlComponent {
-    constructor();
-    push(latentException: LatentStyleException): void;
-}
